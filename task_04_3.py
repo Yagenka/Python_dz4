1ed694c19a5ad6bbeac6722ea1ef62caebdc0b56
Tamara <tomritol@gmail.com>
2022-08-02 11:05:47 +0300

    Список неповторяющихся элементов исходной последовательности

--- task_04_3.py
+++ task_04_3.py
@@ -8,6 +8,15 @@ my_list = my_number.split()
 for i in range(len(my_list)):
     my_list[i] = int(my_list[i])
 
-my_set = set(my_list)
-my_new_list = list(my_set)
+my_new_list = []
+
+for i in my_list:
+    count = 0
+    for j in my_list:
+        if i == j:
+            count += 1
+    if count == 1:
+        my_new_list.append(i)
+
+
 print(f'Список неповторяющихся элементов исходной последовательности - {my_new_list}')
